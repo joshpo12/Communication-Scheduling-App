d7593522fa299204ba8ebf1adba9cec82aafe586
Kevin Nguyen <kevin373_@hotmail.com>
2021-02-08 19:13:44 -0600

    Added stack navigator to App.js in order for correct screen navigation and restrictions

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+
+// import { ContactStackNavigator } from "./navigation/StackNavigator";
+
+// const Drawer = createDrawerNavigator();
+
+// const DrawerNavigator = () =>{
+//     return(
+//         <Drawer.Navigator>
+//             <Drawer.Screen name=""
+//         </Drawer.Navigator>
+//     )
+// }
---
+++ navigation/StackNavigator.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack'
+
+// import Login from '../screens/Login';
+// import AnimatedLoader from '../screens/AnimatedLoader'
+// import AboutMe from '../screens/AboutMe'
+// import AuctionForm from '../screens/Auction_Form'
+// import SubmitAssignment from '../screens/SubmitAssignment'
+// import Registration from '../screens/Registration'
+// import Scheduling from '../screens/Scheduling'
+// import Messager from '../screens/messager'
+// import Main from '../screens/MainPage'
+
+// const Stack = createStackNavigator();
+
+// const MainStackNavigator = () => {
+//     return (
+//         <Stack.Navigator>
+//             <Stack.Screen name='Login' component={Login} />
+//             <Stack.Screen name='Registration' component={Registration} />
+//         </Stack.Navigator>
+//     );
+// }

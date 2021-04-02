80fbda37a0f28af7de3b18a4bead9794eccc04da
Jessica Wheeler <jwheeler10@cse-sd-25.home>
2021-04-01 19:34:36 -0500

    cleaning up scheduler

--- scheduler_module/EventCalendar.js
+++ scheduler_module/EventCalendar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, Button, FlatList, Alert } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Alert, TouchableOpacity } from 'react-native';
 import { List, Divider } from 'react-native-paper';
 import { Calendar } from 'react-native-calendars';
 import { firestore } from 'firebase';
@@ -9,6 +9,7 @@ export default function EventCalendar({navigation}) {
 
     const [events, setEvents] = useState([]);
 
+    // firebase to get events from calendarEvents collection
     useEffect(() => {
         const unsubscribe = firestore()
             .collection('calendarEvents')
@@ -28,6 +29,7 @@ export default function EventCalendar({navigation}) {
             return () => unsubscribe();
     }, []);
 
+    // method to add new rsvp to rsvpCount and push to firebase
     function handleRSVP(item) {
         firestore()
           .collection('calendarEvents')
@@ -42,22 +44,27 @@ export default function EventCalendar({navigation}) {
   }
 
 return(
-    <View style={styles.topPadding}>
+    <View>
         <Calendar 
           onDayPress={(day) => {navigation.navigate('ShowEvent', { chosenDay: day.dateString }), console.log('selectedDay: ', day.dateString)}}
         />
-        <Button
-          onPress={() => navigation.navigate('AddEvent')}
-          title="Add New Event">
-        </Button>
+        <Text style={styles.topText}>Click on a date to see scheduled events by day</Text>
+        {/* button leading to form to add a new event */}
+        <TouchableOpacity style={styles.editProfileBtn} activeOpacity = {.5} 
+            onPress={()=> navigation.navigate('AddEvent')}>
+                <Text style={styles.buttonText}>Add a New Event</Text>
+            </TouchableOpacity>
         <Text style={styles.upcomingEvents}>Upcoming Events: </Text>
         <Text style={styles.RsvpToEvents}>Click on an event to RSVP</Text>
+
+        {/* list of events from firebase */}
         <FlatList
             scrollEnabled='true'
             data={events}
             keyExtractor={item => item._id}
             ItemSeparatorComponent={() => <Divider />}
             renderItem={({item}) => (
+              // when event is pressed, handleRSVP method is called
               <List.Item
                   onPress={() => handleRSVP(item)}
                   title={item.eventName}
@@ -74,10 +81,8 @@ return(
 
 }
 
+// css to style buttons and text
 const styles = StyleSheet.create({
-    topPadding: {
-      paddingTop: 50
-    },
     upcomingEvents: {
       fontSize: 25,
       textAlign: 'center',
@@ -100,5 +105,26 @@ const styles = StyleSheet.create({
       height: 15,
       flex: 1,
       paddingTop: 30
-    }
+    },
+    editProfileBtn: {
+      alignItems: 'center',
+      marginTop: 10,
+      paddingTop: 15,
+      paddingBottom: 15,
+      marginLeft: 145,
+      marginRight: 145,
+      backgroundColor: '#E1B426',
+      borderRadius: 30,
+      borderWidth: 1,
+      borderColor: '#fff'
+  },
+  buttonText: {
+      textAlign: 'center'
+  },
+  topText: {
+    fontSize: 15,
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
   });
--- scheduler_module/ShowEvent.js
+++ scheduler_module/ShowEvent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, Button, Modal, FlatList, Alert } from 'react-native';
+import { View, StyleSheet, Text, Button, Modal, FlatList, Alert, TouchableOpacity } from 'react-native';
 import { List, Divider } from 'react-native-paper';
 import { firestore } from 'firebase';
 import * as firebase from 'firebase';
@@ -8,8 +8,10 @@ export default function ShowEvent({route, navigation}) {
 
     const [events, setEvents] = useState([]);
 
+    // chosen day from EventCalendar
     const { chosenDay } = route.params;
 
+    // pulls events from firebase that match the selected calendar day
     useEffect(() => {
         const unsubscribe = firestore()
             .collection('calendarEvents')
@@ -29,6 +31,7 @@ export default function ShowEvent({route, navigation}) {
             return () => unsubscribe();
     }, []);
 
+    // handle rsvp function (same as in EventCalendar)
     function handleRSVP(item) {
       firestore()
         .collection('calendarEvents')
@@ -44,6 +47,7 @@ export default function ShowEvent({route, navigation}) {
 
     return(
         <View>
+          {/* creates sliding modal */}
             <Modal
               transparent={false}
               animationType={"slide"}
@@ -51,6 +55,8 @@ export default function ShowEvent({route, navigation}) {
               <View>
                 <View style={{backgroundColor: "000000aa", margin: 50, padding: 40, borderRadius: 10}}>
                 <Text style={styles.title}>Events on {chosenDay}</Text>
+
+                {/* list of events for selected day (are selectable like events on EventCalendar) */}
                 <FlatList
                     data={events}
                     keyExtractor={item => item._id}
@@ -65,7 +71,15 @@ export default function ShowEvent({route, navigation}) {
                     </List.Item>
                     )}
                 />
-                <Button title="Close Form" onPress={() => navigation.goBack()}></Button>
+
+                {/* button to close form */}
+                <TouchableOpacity
+                        style = {styles.submitFormButton}
+                        activeOpacity = {.5}
+                        onPress = {() => navigation.goBack()}
+                        >
+                            <Text style = {styles.buttonText}> Close Form</Text>
+                  </TouchableOpacity>
                 </View>
               </View>
             </Modal>
@@ -73,9 +87,24 @@ export default function ShowEvent({route, navigation}) {
     )
 }
 
+// css to style buttons and text
 const styles = StyleSheet.create({
   title: {
     fontSize: 25,
     textAlign: 'center' 
-  }
+  },
+  submitFormButton: {
+    alignItems: 'center',
+    marginTop: 10,
+    paddingBottom: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#F5B0C2',
+    borderRadius: 30,
+    borderColor: '#fff'
+  },
+  buttonText: {
+    marginTop: 10,
+    textAlign: 'center'
+ }
 })
